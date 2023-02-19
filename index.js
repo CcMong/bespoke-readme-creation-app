64b596ca61b08e34f739f77db0f1ca1b0ed2c529
CcMong <fullstackcodex@gmail.com>
2023-02-19 17:05:55 +0000

    Copied in the inquirer code from the docs into the init() function in index.js.

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ function writeToFile(fileName, data) {
 // function to initialize program
 function init() {
 
+    inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
+
 }
 
 // function call to initialize program
