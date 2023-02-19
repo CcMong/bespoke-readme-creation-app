3c98ba1c2fb1f199e4c2416eed849fe78668d824
CcMong <fullstackcodex@gmail.com>
2023-02-19 17:09:54 +0000

    Created the array of objects for the questions to be used to prompt the app user, for information that will be used to generate the README file.

--- index.js
+++ index.js
@@ -6,6 +6,61 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
 
+    // Title
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    // Description
+    {
+        type: "input",
+        name: "description",
+        message: "Give a description of your project."
+    },
+    // Installation
+    {
+        type: "input",
+        name: "installation",
+        message: "Provide installation instructions for your project."
+    },
+    // Usage
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide the requisite usage information for your project."
+    },
+    // License
+    {
+        type: "list", // list - can only choose one, checkbox - you can choose multiple
+        choices: [],
+        name: "license",
+        message: "What is the project license type?" // Want to use this answer to create a badge, and have a license notice
+    },
+    // Contributing
+    {
+        type: "input",
+        name: "contributing",
+        message: "Provide contribution guidelines for the project."
+    },
+    // Tests
+    {
+        type: "input",
+        name: "tests",
+        message: "Provide test instructions for the project."
+    },
+    // Questions
+    {
+        type: "input",
+        name: "github",
+        message: "To receive questions and feedback, what is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?" // Add further message on how to reach author if any questions
+    }
+
 ];
 
 // function to write README file
@@ -16,9 +71,7 @@ function writeToFile(fileName, data) {
 function init() {
 
     inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
+  .prompt(questions)
   .then((answers) => {
     // Use user feedback for... whatever!!
   })
