aba20dbfeccfda6bc119b0ef5c5669ffb2b81a72
CcMong <fullstackcodex@gmail.com>
2023-02-20 11:08:59 +0000

    Wrote the Table of Contents part of the template, with links to the different README sections.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -26,6 +26,15 @@ function generateMarkdown(data) {
 
 ${data.description}
 
+## Table of Contents
+[Description](#description)  
+[Installation](#installation)  
+[Usage](#usage)  
+[License](#license)  
+[Contributing](#contributing)  
+[Tests](#tests)  
+[Questions](#questions)
+
 
 <div id="installation"></div>  
 
